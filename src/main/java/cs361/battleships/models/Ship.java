435923e9f44b32d308ad2c7687a580a7b5bd5c00
Ben Geyer <bgeyer1234@gmail.com>
2019-01-20 16:35:23 -0800

    Added code for placing ships and finished sunk test

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -6,19 +6,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
+	private String kind;
+	private int size;
 
 	@JsonProperty private List<Square> occupiedSquares;
 
 	public Ship() {
 		occupiedSquares = new ArrayList<>();
 	}
-	
+
 	public Ship(String kind) {
-		//TODO implement
+
+		this();
+		if(kind == "MINESWEEPER"){
+			this.kind = kind;
+			this.size = 2;
+
+		}
+		else if(kind == "DESTROYER"){
+			this.kind = kind;
+			this.size = 3;
+		}
+		else if (kind == "BATTLESHIP"){
+			this.kind = kind;
+			this.size = 4;
+		}
+
 	}
 
+	public void setOccupiedSquares(List<Square> s){
+		this.occupiedSquares = s;
+	}
+	public int getSize(){
+
+		return size;
+	}
 	public List<Square> getOccupiedSquares() {
-		//TODO implement
-		return null;
+		return occupiedSquares;
 	}
 }
