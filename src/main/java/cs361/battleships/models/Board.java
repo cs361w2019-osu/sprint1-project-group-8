4e4725a81d638d77f6b4f3de21772d22a80c9642
Ben Geyer <bgeyer1234@gmail.com>
2019-01-20 16:20:03 -0800

    Added placing ship code from other branch so tests can work

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -14,14 +14,80 @@ public class Board {
 	 */
 	public Board() {
 		// TODO Implement
+		Ships = new ArrayList<>();
+		Attacks = new ArrayList<>();
 	}
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
-		// TODO Implement
-		return false;
+		List<Square> newSquares = new ArrayList<>();
+
+		int size = ship.getSize();
+
+		//Check if the ship will be placed off of the board
+		if(x < 1 || x + size-1 > 10 || y < 'A' || y + size-1 > 'J')
+			return false;
+
+		//check if a ship overlaps
+		for(int i = 0; i < getShips().size(); i++){
+			for(int j = 0; j < (getShips().get(i).getOccupiedSquares()).size(); j++){
+				for(int k = 0; k < size; k++){
+					if(isVertical){
+						if(((((getShips().get(i).getOccupiedSquares()).get(j)).getRow()) == (x+k)) && ((((Ships.get(i).getOccupiedSquares()).get(j)).getColumn()) == y) ){
+							return false;
+						}
+					}else{
+						if(((getShips().get(i).getOccupiedSquares()).get(j).getRow() == (x )) && ((Ships.get(i).getOccupiedSquares()).get(j).getColumn() == y+k )){
+							return false;
+						}
+					}
+
+				}
+
+			}
+		}
+
+
+		//Create the squares the ship will occupy
+		for(int i = 0; i < size; i++){
+			Square s = new Square(x,y);
+			if(isVertical){
+				s.setRow(x+i);
+				s.setColumn(y);
+			}
+			else{
+				s.setRow(x);
+				int nextValue = (int)y + i; // find the int value plus 1
+				char c = (char)nextValue;
+				s.setColumn(c);
+			}
+			newSquares.add(s);
+
+
+		}
+		ship.setOccupiedSquares(newSquares);
+
+		//Check to make sure the ship was not placed diagonally
+		for(int i = 0; i < size-1; i++){
+			if(isVertical){
+				//checks for a straight line
+				if(ship.getOccupiedSquares().get(i).getColumn() == y+1){
+					return false;
+				}
+
+			}
+			else{
+				if(ship.getOccupiedSquares().get(i).getRow() == x+1){
+					return false;
+				}
+			}
+		}
+
+		Ships.add(ship);
+
+		return true;
 	}
 
 	/*
@@ -49,11 +115,12 @@ public class Board {
 
 	public List<Ship> getShips() {
 		//TODO implement
-		return null;
+		return this.Ships;
 	}
 
 	public void setShips(List<Ship> ships) {
 		//TODO implement
+		this.Ships = ships;
 	}
 
 	public List<Result> getAttacks() {
